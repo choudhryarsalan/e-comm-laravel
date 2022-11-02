3eb8ae5c8018fb5bdb6a0316bf015f431d7ab6c9
Choudhry Arsalan <choudhryarslan1998@gmail.com>
2022-11-02 21:39:44 +0500

    Header, Footer and Login Page done

---
+++ resources/views/master.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/laravel work/E-commerce/resources/css/form.css">
+    <title>E-Commerce Project</title>
+
+
+</head>
+<body>
+
+    {{View::make("header")}}
+    @yield('content')
+    {{View::make("footer")}}
+
+
+
+
+
+ {{-- <Script></Script>    --}}
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
+ <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"     ></script>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script><script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
